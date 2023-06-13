42f1c2e97c98c7db97d8d7f8bf57fba8123ab8f6
Daniela Moreno <dani9292@hotmail.com>
2023-06-13 13:06:04 -0500

    Add table of medical invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -30,4 +30,12 @@ CREATE TABLE
         FOREIGN KEY (invoice_id) REFERENCES patients(id),
         FOREIGN KEY (treatment_id) REFERENCES treatment(id),
     );
+
+CREATE TABLE
+    invoices(
+        id INT PRIMARY KEY,
+        total_amount DECIMAL(10,2),
+        payed_at DATE,
+        FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    );
     
