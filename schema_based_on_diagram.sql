4d757e33a83c8d4e4414d5c3af5b22b6bf275a9d
Daniela Moreno <dani9292@hotmail.com>
2023-06-13 13:04:41 -0500

    Add table of medical invoice items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,8 +16,18 @@ CREATE TABLE
 
 CREATE TABLE 
     medical_histories(
-        FOREIGN KEY (id) REFERENCES invoices(medical_history_id),
+        id INT PRIMARY KEY,
         admitted_at DATE,
         FOREIGN KEY (patient_id) REFERENCES patients(id),
         status VARCHAR(250),
     );
+
+CREATE TABLE
+    invoice_items(
+        id INT PRIMARY KEY,
+        unit_price DECIMAL(10,2),
+        quantity INT,
+        FOREIGN KEY (invoice_id) REFERENCES patients(id),
+        FOREIGN KEY (treatment_id) REFERENCES treatment(id),
+    );
+    
