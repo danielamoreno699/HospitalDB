00e7b06f8c7319d4855e79dc2c0307d161ea2f98
Daniela Moreno <dani9292@hotmail.com>
2023-06-13 12:58:21 -0500

    Add table of medical histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -13,3 +13,11 @@ CREATE TABLE
         name VARCHAR(250),
         name VARCHAR(250)
     );
+
+CREATE TABLE 
+    medical_histories(
+        FOREIGN KEY (id) REFERENCES invoices(medical_history_id),
+        admitted_at DATE,
+        FOREIGN KEY (patient_id) REFERENCES patients(id),
+        status VARCHAR(250),
+    );
