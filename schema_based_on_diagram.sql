3718d0166d20430e14137f86b146d023274250c9
Daniela Moreno <dani9292@hotmail.com>
2023-06-13 14:47:27 -0500

    included missing name and included indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,6 +12,7 @@ CREATE TABLE
 CREATE TABLE
     treatment(
         id INT PRIMARY KEY,
+        type VARCHAR(250),
         name VARCHAR(250),
     );
 
@@ -55,6 +56,8 @@ CREATE INDEX fk_medical_histories_patients_idx ON medical_histories(patient_id);
 CREATE INDEX fk_invoice_items_invoices_idx ON invoice_items(invoice_id);
 CREATE INDEX fk_invoice_items_treatments_idx ON invoice_items(treatment_id);
 CREATE INDEX fk_invoices_medical_histories_idx ON invoices(medical_history_id);
+CREATE INDEX fk_medical_histories_treatments_medical_histories_idx ON medical_histories_treatments(medical_history_id);
+CREATE INDEX fk_medical_histories_treatments_treatments_idx ON medical_histories_treatments(treatment_id);
 
 
 
